a6bae08c54da9a294bd8a80401824e75c1592d9b
Fabricio Pirini <fabricio.pirini@gmail.com>
2020-08-12 16:18:31 +0200

    Project structure and login page

---
+++ src/pages/Signin/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { FiLogIn } from "react-icons/fi";
+
+import logoImg from "../../assets/logo.svg"
+
+import { Container, Content, Background } from "./styles";
+
+const Signin: React.FC = () => (
+  <Container>
+    <Content>
+      <img src={logoImg} alt="GoBarber" />
+
+      <form>
+        <h1>Faca seu logon</h1>
+        <input placeholder="E-mail" />
+        <input type="password" placeholder="Senha"></input>
+        <button type="submit">Entrar</button>
+        <a href="forgot">Esqueci minha senha</a>
+      </form>
+      <a href="login">
+        <FiLogIn />
+        Criar conta
+      </a>
+
+    </Content>
+    <Background />
+
+  </Container>
+)
+
+export default Signin
